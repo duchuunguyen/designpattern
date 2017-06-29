501040963fcf5080f4a3e165f9d917417740022f
Nguyen Huu Duc <duchuunguyen@Nguyens-MacBook-Pro.local>
2017-06-28 21:13:18 -0400

    singleton pattern

---
+++ src/main/java/learning/edu/designpattern/chapter05/ChocolateBoiler.java
@@ -0,0 +1,49 @@
+package learning.edu.designpattern.chapter05;
+
+/**
+ * Created by duchuunguyen on 6/12/17.
+ */
+public class ChocolateBoiler {
+    private static ChocolateBoiler chocolateBoiler;
+    private boolean empty;
+    private boolean boiled;
+
+    public static ChocolateBoiler getInstance() {
+        if (chocolateBoiler == null) {
+            chocolateBoiler = new ChocolateBoiler();
+        }
+        return chocolateBoiler;
+    }
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            empty = true;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
